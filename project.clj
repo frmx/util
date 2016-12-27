4151b4c26bf7dfc78efec61136c411dc6a727c69
Donagh MacEan <bobber@forgotten.roads.mx>
2016-12-26 22:01:24 -0600

    Updated for landing app.

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject mx.roads.forgotten/admin "0.0.1-SNAPSHOT"
-  :description "The FRMX Admin Pages"
-  :url "https://gitlab.com/forgotten-roads/auth"
+(defproject mx.roads.forgotten/landing "0.0.1-SNAPSHOT"
+  :description "The FRMX Landing Page"
+  :url "https://github.com/forgotten-roads/landing"
   :scm {
     :name "git"
-    :url  "https://gitlab.com/forgotten-roads/auth"}
+    :url  "https://github.com/forgotten-roads/landing"}
   :license {
     :name "Apache License, Version 2.0"
     :url "http://www.apache.org/licenses/LICENSE-2.0"}
@@ -18,16 +18,13 @@
     [ring/ring-defaults "0.3.0-beta1"]
     [ring/ring-devel "1.6.0-beta6"]
     [clj-http "3.3.0"]
-    ;; Authentication and Authorization
-    [com.cemerick/friend "0.2.3" :exlusions [clj-http]]
-    [friend-google-openid "0.2.1"]
     ;; Database
     [com.taoensso/carmine "2.15.0"]
     ;; Messaging
     [clojurewerkz/machine_head "1.0.0-beta11-SNAPSHOT"]
     ;; Support & Util libs
-    [clojure-ini "0.0.2"]
-    [clojusc/twig "0.2.6"]
+    [clojusc/env-ini "0.1.0"]
+    [clojusc/twig "0.3.0"]
     [com.github.jnr/jnr-posix "3.0.31"]
     [com.stuartsierra/component "0.3.1"]
     [potemkin "0.4.3"]]
@@ -48,10 +45,10 @@
       :plugins [
         [lein-codox "0.10.1"]]
       :codox {
-        :project {:name "FRMX App Server"}
+        :project {:name "FRMX Landing Page"}
         :output-path "docs/master/current"
         :doc-paths ["docs/source"]
-        :namespaces [#"^mx.roads.forgotten.app-server\."]
+        :namespaces [#"^mx.roads.forgotten\."]
         :metadata {
           :doc/format :markdown
           :doc "Documentation forthcoming"}}}
@@ -60,6 +57,6 @@
         [org.clojure/tools.namespace "0.2.11" :exclusions [org.clojure/clojure]]
         [lein-simpleton "1.3.0" :exclusions [org.clojure/clojure]]]
       :source-paths ["dev-resources/src"]
-      :repl-options {:init-ns mx.roads.forgotten.app-server.dev}
-      :aot [mx.roads.forgotten.app-server.dev.server]
-      :main mx.roads.forgotten.app-server.dev.server}})
+      :repl-options {:init-ns mx.roads.forgotten.landing.dev}
+      :aot [mx.roads.forgotten.landing.server]
+      :main mx.roads.forgotten.landing.server}})
